ced1887d2a7d9e1cb0864d235529437bcf379724
Avanti <avanti@localhost.localdomain>
2015-12-16 18:17:04 +0530

    encryption code

--- serviceServer/admin_server.py
+++ serviceServer/admin_server.py
@@ -15,40 +15,64 @@ import hmac
 import user
 import sys
 import os
+from Crypto.PublicKey import RSA
+from Crypto import Random
 
 i=0
 
 
 @bottle.post('/newNode')
 def insert_entry():
-    postdata = request.body.read()
-    print postdata #this goes to log file only, not to client
-    name,location=postdata.split("=",1)
-    pwd="pwd"+name+location
-    cnx=mysql.connector.connect(user="ideate",password='password',database='one')
-    cursor=cnx.cursor()
-    try:
-        add_entry=("INSERT INTO data (name,location,pwd) VALUES (%s,%s,%s)")
-	entry_data=(name,location,pwd)
-	cursor.execute(add_entry,entry_data)
-	cnx.commit()
-	cursor.close()
-	cnx.close()
-	
-	#add encryption
+	postdata = request.body.read()
+   	print postdata #this goes to log file only, not to client
+    	name,location=postdata.split("=",1)
+    	pwd="pwd"+name+location
 
-	return "1"
+	#encryption
+    	file=open("../edgeDevice/admin/keys_client.txt","r")
+	public_str=file.read()
+	file.close()
+	public_key=RSA.importKey(public_str)
+	enc_data=public_key.encrypt(pwd, 32)
+
+	#entering into db
+    	cnx=mysql.connector.connect(user="ideate",password='password',database='one')
+    	cursor=cnx.cursor()
+    	try:
+        	add_entry=("INSERT INTO data (name,location,pwd) VALUES (%s,%s,%s)")
+		entry_data=(name,location,pwd)
+		cursor.execute(add_entry,entry_data)
+		cursor.execute("select uid from data where pwd='"+pwd+"'")
+		uid=cursor.fetchone()
+		print uid
+		cnx.commit()
+		cursor.close()
+		cnx.close()
+		# concat uid to enc_pwd
+		return enc_data
 		
-    except:
-        print ("Error inserting post")
-	return "0"
+    	except:
+        	print ("Error inserting post")
+		return "Invalid"
 
 
 
 @bottle.post('/login') 
 def do_login(): 
+	random_num1=Random.new().read
+	keys1=RSA.generate(1024,random_num1)
+	public_key1=keys1.publickey()
+	
+	#creating server keys
+	file=open("keys_server.txt","w+")
+	file.write(public_key1.exportKey())
+	file.close()
+	#reading encrypted data from client 
 	postdata=request.body.read()
-	uid,pwd=postdata.split("=",1)
+	uid,enc_pwd=postdata.split("=",1)
+	#decrypting pwd
+	pwd=keys1.decrypt(enc_pwd)
+	
 	print uid+" "+pwd
 	cnx=mysql.connector.connect(user="ideate",password='password',database='one')
     	cursor=cnx.cursor()
